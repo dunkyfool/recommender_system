7ac35c4da8b7ceb96d9d157086fc57f775323cc5
dunkyfool <dunkyfool0113@gmail.com>
2016-10-22 03:27:19 +0800

    init OK

--- US_dnn.py
+++ US_dnn.py
@@ -1,94 +1,67 @@
 import tensorflow as tf
+from utils.initParams import *
+from utils.layers import *
 import numpy as np
 
-# Parameters
-learning_rate = 0.01
-training_epochs = 20
-batch_size = 256
-display_step = 1
-examples_to_show = 10
+class US_dnn:
+  '''
+  '''
+  def __init__(self, input):
+    pass
+    self.input = input
+    self.weight, self.bias = {}, {}
+    self.weight['en_w1'] = weight([8,6],'en_w1')
+    self.weight['en_w2'] = weight([6,4],'en_w2')
+    self.weight['en_w3'] = weight([4,2],'en_w3')
+    self.weight['en_b1'] = bias([6],'en_b1')
+    self.weight['en_b2'] = bias([4],'en_b2')
+    self.weight['en_b3'] = bias([2],'en_b3')
 
-# Network Parameters
-n_hidden_1 = 256 # 1st layer num features
-n_hidden_2 = 128 # 2nd layer num features
-n_input = 784 # MNIST data input (img shape: 28*28)
+    self.weight['de_w1'] = tf.transpose(self.weight['en_w3'])
+    self.weight['de_w2'] = tf.transpose(self.weight['en_w2'])
+    self.weight['de_w3'] = tf.transpose(self.weight['en_w1'])
+    self.weight['de_b1'] = bias([4],'de_b1')
+    self.weight['de_b2'] = bias([6],'de_b2')
+    self.weight['de_b3'] = bias([8],'de_b3')
 
-X = tf.placeholder("float", [None, n_input])
+    self.weight['qq']=True
 
-weights = {
-        'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-        'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-        'decoder_h1': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_1])),
-        'decoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_input])),
-}
-biases = {
-        'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
-        'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2])),
-        'decoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
-        'decoder_b2': tf.Variable(tf.random_normal([n_input])),
-}
+  def encoder(en_input):
+    pass
+    layer1 = dnn(en_input, self.weight['en_w1'],self.weight['en_b1'])
+    layer2 = dnn(layer1, self.weight['en_w1'],self.weight['en_b1'])
+    layer3 = dnn(layer2, self.weight['en_w1'],self.weight['en_b1'])
+    return layer3
 
+  def decoder(de_input):
+    pass
+    layer1 = dnn(de_input, self.weight['de_w1'],self.weight['de_b1'])
+    layer2 = dnn(layer1, self.weight['de_w1'],self.weight['de_b1'])
+    layer3 = dnn(layer2, self.weight['de_w1'],self.weight['de_b1'])
+    return layer3
 
-def encoder(x):
-    # Encoder Hidden layer with sigmoid activation #1
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
-                                                biases['encoder_b1']))
-    # Decoder Hidden layer with sigmoid activation #2
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
-                                                      biases['encoder_b2']))
-    return layer_2
+  def noise(range=[3,5]):
+    '''
+    1. jiggle correct answer
+    '''
+    pass
 
-def decoder(x):
-    # Encoder Hidden layer with sigmoid activation #1
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
-                                                biases['decoder_b1']))
-    # Decoder Hidden layer with sigmoid activation #2
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
-                                                      biases['decoder_b2']))
-    return layer_2
+  def run():
+    '''
+    1. connect model
+    2. init/sess
+    '''
+    pass
 
-# Construct model
-encoder_op = encoder(X)
-decoder_op = decoder(encoder_op)
+  def selftest():
+    '''
+    1. share variable(learning, identity)
+    2. limited level(pass back to control)
+    '''
+    pass
 
-# Prediction
-y_pred = decoder_op
-# Targets (Labels) are the input data.
-y_true = X
-
-
-# Define loss and optimizer, minimize the squared error
-cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
-optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
-
-# Initializing the variables
-init = tf.initialize_all_variables()
-
-with tf.Session() as sess:
-    sess.run(init)
-    total_batch = int(mnist.train.num_examples/batch_size)
-    # Training cycle
-    for epoch in range(training_epochs):
-        # Loop over all batches
-        for i in range(total_batch):
-            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
-            # Display logs per epoch step
-            if epoch % display_step == 0:
-                print("Epoch:", '%04d' % (epoch+1),
-                      "cost=", "{:.9f}".format(c))
-
-    print("Optimization Finished!")
-
-    # Applying encode and decode over test set
-    encode_decode = sess.run(
-        y_pred, feed_dict={X: mnist.test.images[:examples_to_show]})
-    # Compare original images with their reconstructions
-    f, a = plt.subplots(2, 10, figsize=(10, 2))
-    for i in range(examples_to_show):
-        a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
-        a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
-    f.show()
-    plt.draw()
-    plt.waitforbuttonpress()
+if __name__=='__main__':
+  data = np.array([1,2,3,4,5,6,7,8,
+                   2,2,3,4,5,6,7,8]).reshape(2,8)
+  x = US_dnn(data)
+  print x.weight['en_w1'], x.weight['en_w1'].get_shape()
